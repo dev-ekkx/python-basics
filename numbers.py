0807d303ec841ea63d2451771e219a5279950ad6
Dev Ekkx <devekkx@gmail.com>
2025-10-27 18:07:11 +0000

    feature: implement number functions

--- numbers.py
+++ numbers.py
@@ -17,3 +17,11 @@ print(10/2) # Division
 print(10//3) # Floor Division
 print(10%3) # Modulus
 print(10**2) # Exponentiation
+
+# Number functions
+print(abs(-7)) # Absolute value
+print(round(4.6)) # Rounding
+print(pow(3, 4)) # Power function
+print(max(5, 10)) # Maximum value
+print(min(5, 10)) # Minimum value
+
