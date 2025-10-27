afe4078a2a296fedb128ca4c038bf1ec14ee4c3c
Dev Ekkx <devekkx@gmail.com>
2025-10-27 17:59:02 +0000

    feature: string implementation

--- strings.py
+++ strings.py
@@ -1,4 +1,3 @@
-# Single line strings
 title = "Python Basics"
 
 message = """
@@ -9,3 +8,8 @@ You can include line breaks,
 """
 print(len(title))
 print(title[0:10])
+
+first_name = "John"
+last_name = "Doe"
+full_name = f"{first_name} {last_name}"
+print(full_name.upper())
