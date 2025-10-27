21aac75bcfed0a93398582c66c1945241b2e8aa8
Dev Ekkx <devekkx@gmail.com>
2025-10-27 17:53:25 +0000

    feature: strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+# Single line strings
+title = "Python Basics"
+
+message = """
+This is a multi-line string.
+It can span multiple lines.
+
+You can include line breaks,
+"""
+print(len(title))
+print(title[0:10])
